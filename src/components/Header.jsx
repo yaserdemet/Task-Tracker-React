4b6badfec9899a5a8956d29300a77626611537b1
yaser <ddemety372@gmail.com>
2022-07-06 01:56:14 +0300

    nicely done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,13 @@ import "../App.css";
 import { ImCheckboxChecked } from "react-icons/im";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  const [data1, setData1] = useState([]);
-  const [cevir, setCevir] = useState(false);
+  const [isOpen, setIsOpen] = useState(true); //? componenti açıp kapatmak için kullanıldı.
+  const [data1, setData1] = useState([]);     //* gelecek olan inputları tutmak için kullanıldı.
+  const [cevir, setCevir] = useState(false);  //! classlar için kullanıldı.
 
   const handleDelete = (id) => {
     setData1(data1.filter((item) => item.id !== id));
+    // ! silme işlemi ile id ye bakarak gelen id kalanlara eşit olmayanları filter ettim.
     console.log(id);
     console.log(data1);
   };
@@ -56,6 +57,8 @@ const Header = () => {
         )}
       </button>
       {!isOpen && <Inputs data1={data1} setData1={setData1} />}
+
+      //! en önemli kısım burası. Burada yukarıda oluşturduğumuz kompenentteki verileri alıp burada sergilemek için yeni bir state oluşturduk. Ve oradan gelen verileri map edip DOM a bastım.
 
       {data1.length == 0 && <p>No task yet</p> }
       {data1.length == 1 && <p>You have 1 task</p> }
--- src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const List = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default List
